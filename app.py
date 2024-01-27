dfd956e1315e998cee9852a503148ee72640257a
jdyang88 <64808996+jdyang88@users.noreply.github.com>
2024-01-28 07:39:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ st.title('Korean Annual Population')
 
 # Set Korean font for matplotlib in Windows environment
 def set_korean_font():
-    plt.rcParams['font.family'] = 'HYPost'
+    plt.rcParams['font.family'] = 'Malgun Gothic'
     plt.rcParams['axes.unicode_minus'] = False
 
 set_korean_font()
 
 # Load the data with updated caching
-@st.cache
+@st.cache_data
 def load_data():
     data = pd.read_csv('200812_202312_Korean_Annual_Population.csv', encoding='euc-kr')
     return data
@@ -32,9 +32,12 @@ years = sorted(set(int(years_pattern.search(col).group(1))
                    for col in data.columns if years_pattern.search(col)), reverse=True)
 default_year = years[0]  # Default to the most recent year
 
-# Sidebar for filters
-year = st.sidebar.selectbox('연도를 선택하세요', years, index=0)
-region = st.sidebar.selectbox('행정구역을 선택하세요', data['행정구역'].unique())
+# Filters on the same line
+col1, col2 = st.columns(2)
+with col1:
+    year = st.selectbox('Select Year', years, index=0)  # Placed in the first column
+with col2:
+    region = st.selectbox('Select Region', data['행정구역'].unique())  # Placed in the second column
 
 # Filtering the data
 filtered_data = data[data['행정구역'] == region]
@@ -52,13 +55,12 @@ year_str = str(year)
 # Sort age groups with special handling for "100세 이상"
 def sort_age_groups(age_group):
     if age_group == "100세 이상":
-        return 100  # Assign a high number for sorting
+        return 100
     else:
-        # Extract the first number for sorting for age ranges like "xx~xx세"
         try:
             return int(age_group.split('~')[0].replace('세', ''))
         except ValueError:
-            return 0  # Default value in case of unexpected format
+            return 0
 
 # Extract and sort age groups from the column names
 age_groups = [col.split('_')[-1] for col in filtered_data.columns if '세' in col and year_str in col]
@@ -75,11 +77,11 @@ total_female_population = "{:,}명".format(female_data.sum())
 # Plotting bar chart
 fig, ax = plt.subplots()
 
-ax.barh(age_groups, -male_data.values, color='blue', label=f'남자: {total_male_population}')
-ax.barh(age_groups, female_data.values, color='red', label=f'여자: {total_female_population}')
+ax.barh(age_groups, -male_data.values, color='blue', label=f'Male: {total_male_population}')
+ax.barh(age_groups, female_data.values, color='red', label=f'Female: {total_female_population}')
 
-ax.set_xlabel('인구 수')
-ax.set_title(f'{region} {year}년 성별 인구 분포')
+ax.set_xlabel('Population')
+ax.set_title(f'{region} Year{year} M(Blue) & F(Red) Distribution')
 plt.legend()
 ax.axvline(x=0, color='grey', linewidth=0.8)
 st.pyplot(fig)
