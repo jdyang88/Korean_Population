a9a6d9d82ca07721f328fcc5def4ff0d084f078b
jdyang88 <64808996+jdyang88@users.noreply.github.com>
2024-01-28 18:50:37 +0900

    Update app.py
    korean fond added

--- app.py
+++ app.py
@@ -2,19 +2,53 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
+import seaborn as sns 
+import numpy as np 
+
+# 한글폰트 적용
+# 폰트 적용
+import os
+import matplotlib.font_manager as fm  # 폰트 관련 용도 as fm
+
+def unique(list):
+    x = np.array(list)
+    return np.unique(x)
+
+@st.cache_data
+def fontRegistered():
+    font_dirs = [os.getcwd() + '/customFonts']
+    font_files = fm.findSystemFonts(fontpaths=font_dirs)
+
+    for font_file in font_files:
+        fm.fontManager.addfont(font_file)
+    fm._load_fontmanager(try_read_cache=False)
+    
+
+def main():
+    
+    fontRegistered()
+    fontNames = [f.name for f in fm.fontManager.ttflist]
+    fontname = st.selectbox("폰트 선택", unique(fontNames))
+
+    plt.rc('font', family=fontname)
+    tips = sns.load_dataset("tips")
+    fig, ax = plt.subplots()
+    sns.scatterplot(data=tips, x = 'total_bill', y = 'tip', hue='day')
+    ax.set_title("한글 테스트")
+    st.pyplot(fig)
+    
+    st.dataframe(tips)
+    
+if __name__ == "__main__":
+    main()
+
+
 
 # Streamlit app title
 st.title('Korean Annual Population')
 
 # Displaying the source attribution
 st.markdown('Source: [Statistics Korea](https://jumin.mois.go.kr/ageStatMonth.do)')
-
-# Set Korean font for matplotlib in Windows environment
-def set_korean_font():
-    plt.rcParams['font.family'] = 'Malgun Gothic'
-    plt.rcParams['axes.unicode_minus'] = False
-
-set_korean_font()
 
 # Load the data with updated caching
 @st.cache
